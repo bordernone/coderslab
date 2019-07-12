6175bcee096d8ef11782da1a4411daba6bebaaec
Bishnu Dev <bishnu.dev52@gmail.com>
2019-07-12 18:02:10 +0545

    Password reset email template
    added custom template for password reset

--- login/urls.py
+++ login/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('native/', loginUser, name='loginuser'),
     path('google/', googleLogin, name="googlelogin"),
     path('facebook/', facebookLogin, name="facebooklogin"),
-    path('recover/', auth_views.PasswordResetView.as_view(template_name='accountrecover/recoveraccount.html'), name="password_reset"),
+    path('recover/', auth_views.PasswordResetView.as_view(template_name='accountrecover/recoveraccount.html', html_email_template_name='accountrecover/passwordresetemailtemplate.html'), name="password_reset"),
     path('recover/done/', auth_views.PasswordResetDoneView.as_view(template_name='accountrecover/done.html'), name='password_reset_done'),
     path('recover/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accountrecover/complete.html'), name='password_reset_complete'),
     url(r'^recover/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(template_name='accountrecover/confirm.html'), name='password_reset_confirm'),
