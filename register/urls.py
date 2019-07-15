0b370fbb99856185352b7f902c7d44f04a208fae
Bishnu Dev <bishnu.dev52@gmail.com>
2019-07-15 18:33:17 +0545

    downgraded from django2.2 to django2.1
    Downgraded from django2.2 to django2.1 due to incompatible version of sqlite on the server. Also had to change urls.py so that djnago 2.1 can work without issue

--- register/urls.py
+++ register/urls.py
@@ -5,5 +5,5 @@ from .views import registerView, registerUser, activate
 urlpatterns = [
     path('', registerView, name='registerView'),
     path('user/', registerUser, name='registerUser'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',activate, name='activate'),
+    path('activate/<uidb64>/<token>/',activate, name='activate'),
 ]
