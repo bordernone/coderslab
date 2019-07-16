5718f74c252a4d9ca86933b17bd11ee146ef3940
Bishnu Dev <bishnu.dev52@gmail.com>
2019-07-16 08:46:52 +0545

    minor changes in logout

--- logout/views.py
+++ logout/views.py
@@ -1,7 +1,9 @@
 from django.http import Http404, HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import logout
+from django.conf import settings
 
 def logoutUser(request):
+    googleclientid = settings.GOOGLE_CLIENT_ID
     logout(request)
-    return render(request, 'logout.html')
+    return render(request, 'logout.html', {'googleclientid': googleclientid})
