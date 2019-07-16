6ab6e1db05896623ffee50be57c432cfccda4c5c
Bishnu Dev <bishnu.dev52@gmail.com>
2019-07-16 10:19:57 +0545

    minor changes on how templates retrieve fbappid

--- login/urls.py
+++ login/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from django.conf.urls import url
 from .views import loginPage, googleLogin, facebookLogin, loginUser
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+
+facebookAppId = settings.FACEBOOK_APP_ID
 
 urlpatterns = [
     path('', loginPage, name='login'),
     path('native/', loginUser, name='loginuser'),
     path('google/', googleLogin, name="googlelogin"),
     path('facebook/', facebookLogin, name="facebooklogin"),
-    path('recover/', auth_views.PasswordResetView.as_view(template_name='accountrecover/recoveraccount.html', html_email_template_name='accountrecover/passwordresetemailtemplate.html'), name="password_reset"),
+    path('recover/', auth_views.PasswordResetView.as_view(template_name='accountrecover/recoveraccount.html', html_email_template_name='accountrecover/passwordresetemailtemplate.html', extra_context = {'fbappid': facebookAppId}) ,name="password_reset"),
     path('recover/done/', auth_views.PasswordResetDoneView.as_view(template_name='accountrecover/done.html'), name='password_reset_done'),
     path('recover/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accountrecover/complete.html'), name='password_reset_complete'),
     url(r'^recover/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(template_name='accountrecover/confirm.html'), name='password_reset_confirm'),
