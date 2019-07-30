cc323b00527e42e12053c917d49b064ce9b12ec4
Bishnu Dev <bishnu.dev52@gmail.com>
2019-07-30 12:35:36 +0545

    fixed score evaluation

--- scoreboard/utils.py
+++ scoreboard/utils.py
@@ -6,3 +6,20 @@ def userObjFromId(id):
         return thisUser
     except:
         return None
+
+# only pass ordered queryset in this function
+def getDistinctModelFields(queryset, fields):
+    noOfFields = len(fields)
+    newqueryset = []
+    usedFields = []
+    for thisObj in queryset:
+        temp = []
+        for i in range(noOfFields):
+            temp.append(getattr(thisObj, fields[i]))
+        if temp in usedFields:
+            pass
+        else:
+            usedFields.append(temp)
+            newqueryset.append(thisObj)
+    
+    return newqueryset
