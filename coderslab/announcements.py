46d2c364f5690478c4a38bbec71f37437a1b8202
Lemon Ads <lemonads@protonmail.com>
2019-12-29 13:48:27 +0545

    Changed homepage desc and removed announcement

--- coderslab/announcements.py
+++ coderslab/announcements.py
@@ -1,12 +1,13 @@
 from django.urls import reverse
 
 def getAnnouncementData():
-    url = reverse('getCertificateView')
-    html = ('<div id="announcement-bar" class="p-2 text-center announcement-bar">' +
-            '<p class="noMargin">' + 
-            ' Get your certificate of participation ' + 
-            '  <a href="' +
-            url +
-            '" class="text-primary">here</a></p>' +
-            '</div>')
-    return html
+#     url = reverse('getCertificateView')
+#     html = ('<div id="announcement-bar" class="p-2 text-center announcement-bar">' +
+#             '<p class="noMargin">' + 
+#             ' Get your certificate of participation ' + 
+#             '  <a href="' +
+#             url +
+#             '" class="text-primary">here</a></p>' +
+#             '</div>')
+#     return html
+    return null
